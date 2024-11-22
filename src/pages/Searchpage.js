dd11f13630b658804fa76d44599dccb760e79ef5
yeon <parkwlfod@naver.com>
2024-11-22 14:02:02 +0900

    s

--- src/pages/Searchpage.js
+++ src/pages/Searchpage.js
@@ -9,7 +9,19 @@ const SearchPage = () => {
   return (
     <div className='searchwrap'>
       <h1>검색 결과 <span>20건</span></h1>
-      
+      {results ? (
+        <ul>
+          {results.results.map(movie => (
+            <li key={movie.id}>
+              <h2>{movie.title}</h2>
+              <p>{movie.overview}</p>
+              <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>검색 결과가 없습니다.</p>
+      )}
     </div>
   );
 }
