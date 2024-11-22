5938ab23687a5314435a91ff21472f6356946abe
yeon <parkwlfod@naver.com>
2024-11-22 13:59:18 +0900

    ser

--- src/pages/Searchpage.js
+++ src/pages/Searchpage.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import '../css/search.scss'
 
-const searchpage = () => {
+const SearchPage = () => {
+  const location = useLocation();
+  const { results } = location.state || { results: null }; // state에서 결과 가져오기
+
   return (
-    <div>searchpage</div>
-  )
+    <div className='searchwrap'>
+      <h1>검색 결과 <span>20건</span></h1>
+      
+    </div>
+  );
 }
 
-export default searchpage
+export default SearchPage;
