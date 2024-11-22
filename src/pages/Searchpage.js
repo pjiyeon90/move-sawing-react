03374272ba64c25a67b32698eeba76aa6c956807
yeon <parkwlfod@naver.com>
2024-11-22 13:50:30 +0900

    sdf

--- src/pages/Searchpage.js
+++ src/pages/Searchpage.js
@@ -1,29 +1,9 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
-import '../css/search.scss'
+import React from 'react'
 
-const SearchPage = () => {
-  const location = useLocation();
-  const { results } = location.state || { results: null }; // state에서 결과 가져오기
-
+const searchpage = () => {
   return (
-    <div className='searchwrap'>
-      <h1>검색 결과 <span>20건</span></h1>
-      {results ? (
-        <ul>
-          {results.results.map(movie => (
-            <li key={movie.id}>
-              <h2>{movie.title}</h2>
-              <p>{movie.overview}</p>
-              <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>검색 결과가 없습니다.</p>
-      )}
-    </div>
-  );
+    <div>searchpage</div>
+  )
 }
 
-export default SearchPage;
+export default searchpage
